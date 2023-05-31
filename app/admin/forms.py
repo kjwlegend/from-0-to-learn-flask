828c984073faac6011ff666a14efa702f304c7a2
jingwei kong <56349014@qq.com>
2023-05-31 09:58:32 +0800

    learn how to add category

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -2,3 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, ValidationError, EqualTo
 from werkzeug.security import check_password_hash
+
+
+class CategoryForm(FlaskForm):
+    name = StringField('分类名称', validators=[DataRequired(), Length(1, 20, message='分类名称长度在1-20个字符之间')])
+    icon = StringField('图标', validators=[Length(1, 50, message='图标长度在1-20个字符之间')])
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, blueprints
 from ..auth.views.auth import login_required
 from ..blog.models import Category, Tag, Post
+from .forms import CategoryForm
+from RealProject import db
 
 bp = blueprints.Blueprint('admin', __name__, url_prefix='/admin', static_folder='static', template_folder='templates')
 
@@ -22,3 +24,32 @@ def category():
     return render_template('admin/category.html',
                            category_list = category_list,
                            pagination = pagination)
+
+@bp.route('/category/add', methods=['GET', 'POST'])
+@login_required
+def category_add():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        category = Category(name=form.name.data, icon=form.icon.data)
+        db.session.add(category)
+        db.session.commit()
+        flash('分类添加成功')
+        return redirect(url_for('admin.category'))
+
+    return render_template('admin/category_form.html', form=form)
+
+@bp.route('/category/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def category_edit(id):
+    category = Category.query.get_or_404(id)
+    form = CategoryForm()
+    if form.validate_on_submit():
+        category.name = form.name.data
+        category.icon = form.icon.data
+        db.session.commit()
+        flash('分类编辑成功')
+        return redirect(url_for('admin.category'))
+
+    form.name.data = category.name
+    form.icon.data = category.icon
+    return render_template('admin/category_form.html', form=form)
